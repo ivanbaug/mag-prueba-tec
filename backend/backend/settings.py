8d55372d579b87045ea260737d676b19e51cea6a
ivanbaug <ivsitioinfo@gmail.com>
2022-03-04 01:27:11 -0500

    add db config and first migration to local server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,10 +84,22 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "mssql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        # "PORT": "1433",
+        "OPTIONS": {
+            "driver": "ODBC Driver 17 for SQL Server",
+            "isolation_level": "READ UNCOMMITTED",  # Prevent deadlocks
+        },
+    },
 }
 
 
