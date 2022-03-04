e4f8ca065c1e144715024afffe696a7d5b14d390
ivanbaug <ivsitioinfo@gmail.com>
2022-03-04 01:03:15 -0500

    model creation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+# Load environment variables when debugging
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+zy(miuqdj(u7=-3*=iwy02eg9g5cgc!(kj7o%%_67#8-_k8jd"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -124,3 +131,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
--- backend/companiesapp/models.py
+++ backend/companiesapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    description = models.TextField(null=True, blank=True)
+    num_employees = models.IntegerField(null=False, blank=False, default=0)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True)
+    department = models.CharField(max_length=200, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
